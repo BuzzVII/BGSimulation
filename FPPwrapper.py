67207446b12561783738b06d9ec3ac58a75749f8
Kristian Weegink <k.weegink@gmail.com>
2013-11-05 21:28:26 +1000

    Modified FitPatient to run batch of patients
    Changed FitPatient to go through all recordings in a directory and
    its subdirectories. This involved lowering the log levels of FPPwrapper
    to reduce the guff on screen as it goes through each file. The results
    from FitPatient.py are written into an xml file leastsq.xml.

--- FPPwrapper.py
+++ FPPwrapper.py
@@ -1,6 +1,6 @@
 # wrapper for an object that opens a patient file finds the power specturm
-# then runs a simulation given C and rate and finds the returns the L2
-# difference of the PSDs
+# then runs a simulation given C and rate and finds the returns the residues
+# of the difference of the PSDs
 #
 # Kristian Weegink, Nov 2013
 
@@ -16,24 +16,24 @@ import Logger as lg
 class Patient:
     
     # intialize by finding the patient PSD
-    def __init__(self,patient_file,logger=lg.logger(1)):
+    def __init__(self,patient_file,logger=lg.logger(0)):
         self.log = logger
         self.sr,self.Vt = wav.read(patient_file)
         self.log.info('patient loaded')
         self.nfft=2**int(math.log(len(self.Vt),2))+1
-        self.Pxx,self.freqs=pylab.psd(x=self.Vt,Fs=self.sr,NFFT=self.nfft/10,window=pylab.window_none, noverlap=100)   
+        self.Pxx,self.freqs=pylab.psd(x=self.Vt,Fs=self.sr,NFFT=self.nfft,window=pylab.window_none, noverlap=1)   
         self.log.info('patient initialized')
 
     # find the L2 difference between the FPP simulation and patient PSDs
     def fpp(self,rate,C):
         self.log.info('C = ' + str(C) + '; rate = ' + str(rate) )
         Vt,t = FPP.FPP(log=lg.logger(0), N=10000, dt=1./24000, distributionParameter=[C,rate], plotAll=False,efield=False)
-        Pxi,freqs = pylab.psd(x=17*Vt,Fs=self.sr,NFFT=self.nfft/10,window=pylab.window_none, noverlap=100)
-        self.log.info('L2  = ' + str(np.sqrt((np.square(np.subtract(np.log(self.Pxx[40:362]),np.log(np.multiply(Pxi[40:362],20))))).sum())))
+        Pxi,freqs = pylab.psd(x=Vt,Fs=self.sr,NFFT=self.nfft,window=pylab.window_none, noverlap=1)
+        self.log.info('L2  = ' + str(np.sqrt((np.square(np.subtract(self.Pxx[370:3300],10000*Pxi[370:3300])).sum()))))
         #pylab.show()
-        #pylab.loglog(np.multiply(Pxi[40:362],20))
-        #pylab.loglog(self.Pxx[40:362])
+        #pylab.loglog(10000*Pxi[37:3300])
+        #pylab.loglog(self.Pxx[37:3300])
         #pylab.show()
         #pylab.plot(self.freqs)
         #pylab.show()
-        return np.subtract(self.Pxx,Pxi)
+        return np.subtract(self.Pxx[37:3300],10000*Pxi[37:3300])
--- FitPatient.py
+++ FitPatient.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python2.7
 # Finds the value of the weibull shape parameter that minimizes
-# the L2 difference of a patient PSD and FPP simulation
+# the residuals of a patient PSD and FPP simulation
 #
 # Kristian Weegink, Nov 2013
 
@@ -12,15 +13,39 @@ import math
 import scipy.io.wavfile as wav
 from scipy.optimize import leastsq
 import Logger as lg
+import os
+import sys
+import fnmatch
 
 log = lg.logger(1)
 log.info('creating patient object')
 
-patient = FPPwrapper.Patient(patient_file='/home/uqkweegi/Documents/Data/dbsdata/OLD MER/Patient32/32_S_L_E1_20.wav');
+fileList = []
 
-def fit_func(x0):
-    return patient.fpp(x0[0],x0[1])
+for path, dirs, files in os.walk(os.path.abspath("/home/uqkweegi/Documents/Data/dbsdata/OLDMER")):
+    for filename in fnmatch.filter(files,"*.wav"):
+        fileList.append(os.path.join(path, filename))
 
-log.info('starting minimization')
-x0 = [30, 0.5]
-Cmin = leastsq(fit_func, x0,full_output=True)
+
+fid = open('/home/uqkweegi/Documents/Data/leastsq.xml','w')
+
+fid.write('<?xml version="1.0" encoding="UTF-8" ?>')
+
+fid.write('<PATIENTFITS>')
+
+
+for fileName in fileList:
+    log.info(fileName)
+    patient = FPPwrapper.Patient(patient_file=fileName);
+
+    def fit_func(x0):
+        return patient.fpp(x0[0],x0[1])
+
+    log.info('starting minimization')
+    x0 = [30, 0.5]
+    Cmin = leastsq(fit_func, x0,full_output=True,xtol=1e-8,ftol=1e-8,epsfcn=0.1)
+    log.info('Weibull parameters A = ' + str(Cmin[0]) + ', B = ' + str(Cmin[1]))
+    fid.write("\t<RECORDING file='"+wavFile+"' a='"+str(Cmin[0])+"' b='"+str(Cmin[0])+" />")
+
+fid.write('</PATIENTFITS>')
+fid.close()
